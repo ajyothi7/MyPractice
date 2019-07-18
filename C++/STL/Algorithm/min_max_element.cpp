fb53b66b5e222e7a1889e176eacb03480cb47d96
ajyothi7 <ajyothirao24@gmail.com>
2019-07-18 13:34:57 +0530

    stl

---
+++ C++/STL/Algorithm/min_max_element.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+//#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+	int arr[] = {10, 500, 20, -10, -100, 50};
+	
+	auto res = min_element((arr + 0), (arr + 6));
+	cout << "min_ele = " << *res << endl;
+	
+	res = max_element((arr + 0), (arr + 6));
+	cout << "max_ele = " << *res << endl;
+	
+	vector<int> vec = {10, 500, 20, -10, -100, 50};
+	
+	auto res2 = min_element(vec.begin(), vec.end());
+	cout << "min_ele = " << *res2 << endl;
+	
+	res2 = max_element(vec.begin(), vec.end());
+	cout << "max_ele = " << *res2 << endl;
+	
+	return 0;
+}
