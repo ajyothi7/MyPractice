b378c869fd4a662028ea8425a70a2098a1ee7950
ajyothi7 <ajyothirao24@gmail.com>
2019-07-29 12:32:35 +0530

    string

--- C++/STL/String.cpp
+++ C++/STL/String.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <set>
 
 using namespace std;
 
@@ -26,10 +27,14 @@ int main(void)
 	if(str1 == str2)
 		cout << "strings are equal" << endl;
 
+	else if(str1 > str2)
+		cout << str1 << " is lexicografically greater" << endl;
+
 	else
-		cout << "strings are not equal" << endl;
+		cout << str2 << " is lexicografically greater" << endl;
 
 	string str3;
+	string str4;
 
 	str3.assign(str1);
 	cout << "str3.assign(str1) - " << str3 <<  endl;
@@ -68,14 +73,58 @@ int main(void)
 	cout << str.substr(4, 10) << endl;
 
 	str1 = "hello";
-	str2 = "hello";
+	str2 = "hiworld";
 	str3 = "world";
+	str4 = "helloworld";
 
 	if(0 == str1.compare(str2))
 		cout << "string are equal" << endl;
 
 	else
 		cout << "string are not equal" << endl;
-		
+
+	if(0 == (str4.compare(5, 5, str3)))
+		cout << "strings are equal" << endl;
+
+	else
+		cout << "strings are not equal" << endl;
+
+	if(0 == (str2.compare(2, 5, str4, 5, 5)))
+		cout << "strings are equal" << endl;
+
+	else
+		cout << "strings are not equal" << endl;
+
+	char c = 'j';
+
+	string s(3, c);
+
+	cout << "char to str : " << s << endl;
+
+	cout << "int to str : " << to_string(1234) << endl;
+
+	cout << "hex string to int using stoi()" << stoi("FF", 0, 16) << endl;
+	cout << "oct string to int using stoi()" << stoi("377", 0, 8) << endl;
+	cout << "binary string to int using stoi()" << stoi("11111111", 0, 2) << endl;
+
+	str1 = "HelloWorld";
+
+	set <char> sObj(begin(str1), end(str1));
+
+	cout << "string to set using set const" << endl;
+	for(auto itr : sObj)
+		cout << itr << " ";
+	cout << endl;
+
+	set <char> sObj1;
+
+	for(auto i : str1)
+		sObj1.insert(i);
+
+	cout << "string to set using insert()" << endl;
+	for(auto itr : sObj1)
+		cout << itr << " ";
+	cout << endl;
+
 	return 0;
 }
